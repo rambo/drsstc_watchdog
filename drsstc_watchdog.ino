313a64ba2313204dd8e58b9f93f3ed983c3f6b2c
Eero af Heurlin <eero.afheurlin@iki.fi>
2014-08-02 18:51:21 +0300

    more code, for some reason both callbacks seem to trigger at both edges

--- drsstc_watchdog.ino
+++ drsstc_watchdog.ino
@@ -1,30 +1,44 @@
+/**
+ * For ATTiny85
+ *
+ * 
+ */
 #include <PinChangeInterruptSimple.h>
 
-#define INPUT_PIN 2
-#define OUTPUT_PIN 1
+#define INPUT_PIN 1
+#define OUTPUT_PIN 2
+// On ATTiny85 the pin numbers correspond to bits on the only port
+#define OUTPUT_PORT PORTB
+#define OUTPUT_BIT 2
+
 
 #define MAX_PULSE_LEN 200 // in uSec
 // TODO: limit the duty-cycle somehow
 
+
 void input_high()
 {
+    OUTPUT_PORT |= _BV(OUTPUT_BIT);
 }
 
 void input_low()
 {
+    OUTPUT_PORT &= 0xff ^ _BV(OUTPUT_BIT);
 }
 
 
 void setup()
 {
-    // put your setup code here, to run once:
-    attachPcInterrupt(2, input_high, RISING);
-    attachPcInterrupt(2, input_low, FALLING);
+    // TODO: disable watchdog
+    pinMode(OUTPUT_PIN, OUTPUT);
+    attachPcInterrupt(INPUT_PIN, input_high, RISING);
+    attachPcInterrupt(INPUT_PIN, input_low, FALLING);
+    // TODO: Setup watchdog (if we can go to usec resolution with it...)
 }
 
 void loop()
 {
-    // put your main code here, to run repeatedly: 
-  
+    // TODO: keep clearing the watchdog
+    // TODO: unless we can enforce max pulse_len with timer keep polling
 }
 
